faac0e671a1e21e6f10f8c58e68b44bf5a6d6bea
steelvelveteen <joeyvico@gmail.com>
2022-01-31 18:33:50 +1030

    dotnet-format

--- Data/Migrations/20220120105901_InitialCreate.cs
+++ Data/Migrations/20220120105901_InitialCreate.cs
@@ -3,30 +3,29 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Nuremberg.Data.Migrations
-{
-    public partial class InitialCreate : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "TestModels",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uuid", nullable: false),
-                    TestModelName = table.Column<string>(type: "text", nullable: false),
-                    TestModelOtherProp = table.Column<string>(type: "text", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_TestModels", x => x.Id);
-                });
-        }
+namespace Nuremberg.Data.Migrations;
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "TestModels");
-        }
+public partial class InitialCreate : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "TestModels",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "uuid", nullable: false),
+                TestModelName = table.Column<string>(type: "text", nullable: false),
+                TestModelOtherProp = table.Column<string>(type: "text", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_TestModels", x => x.Id);
+            });
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "TestModels");
     }
 }
